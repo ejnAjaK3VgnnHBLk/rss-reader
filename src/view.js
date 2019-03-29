74ef66734472a4f18c7b0a24f5bc27c39bb4e1ed
Charles <charles.igel@s.stemk12.org>
2019-03-29 08:50:12 -0600

    Got json parsing to work
    Feed reader still doesn't work

--- src/view.js
+++ src/view.js
@@ -1,44 +1,62 @@
 var fs = require("fs");
 var jsonStr;
-fs.readFile('feeds', 'utf-8', function(err, contents) {
-  jsonStr = contents;
-})
-console.log(jsonStr);
+var path = require('path');
 
-function edit() {
-  const file = "feeds";
-  const jsonfile = require("jsonfile");
-  // Name element
-  var dataone = document.getElementById("yeet").value;
-  // URL element
-  var datatwo = document.getElementById("yeettwo").value;
+var filePath = path.join(__dirname, '../feeds.json');
 
-  // Now i understand
-  console.log(jsonStr);
-  var object = JSON.parse(jsonStr);
-  object['feeds'].push({"name":dataone,"url":datatwo});
-  jsonStr = JSON.stringify(object);
-  console.log(jsonStr);
-
-  fs.writeFile('feeds', jsonStr, (err) => {
-    if (err) {
-      console.error(err);
-      return;
-    };
-    console.log('File has been written');
-  })
-}
+var contentss = fs.readFileSync("feeds.json");
+var jsonContent = JSON.parse(contentss);
 
 
 function rss() {
+  var parser = require("rss-parser-browser");
+  console.log(jsonContent);
   var divt = document.getElementById("feedAddDiv");
   divt.style.display = "none";
   var div = document.createElement("div");
   div.setAttribute("id", "rssFeedDiv");
 
-  var parser = require("rss-parser-browser");
 
-  parser.parseURL("http://rss.slashdot.org/Slashdot/slashdotMain/to", function(err, parsed) {
+  // What does this even do?: console.log(Object.entries(object).sort((a, b) => a-b));
+  // Object
+ /*var leng = object.feeds.length;
+  for (i = 0; i < object.feeds.length; i++) {
+    parser.parseURL(Object.entries(object)[i], function(err, parsed) {
+      document.createTextNode(parsed.feed.title);
+      parsed.feed.entries.forEach(function(entry) {
+        // Create elements.
+        var element = document.createElement("h3");
+        // Creaate id for the h3, so that they aren"t overwritten
+        var titleback = (entry.title).replace(/ /g,"");
+        // Add the id we just made to the h3 tag.
+        element.setAttribute("id", titleback);
+        // Add h3 tag with id to the document.
+        div.appendChild(element);
+        document.body.appendChild(div);
+        // Get article title and url
+        var title = entry.title;
+        // Make text become hyperlink
+        var result = title.link(entry.link);
+        // Add hyperlink to the h3
+        document.getElementById(titleback).innerHTML = result;
+      })
+    })
+  }*/
+
+  // All is working except for here
+  for (var unifiedrl in jsonContent) {
+    // reference as jsonContent[urL]
+    var currentURL = jsonContent[unifiedrl].toString();
+    parser.parseURL(currentURL, function(err, parsed) {
+      console.log(parsed);
+      /*parsed.feed.entries.forEach(function(entry) {
+        console.log('Title: ' + entry.title);
+        console.log('URL: ' + entry.link);
+      })*/
+    })
+  }
+
+/*  parser.parseURL("http://rss.slashdot.org/Slashdot/slashdotMain/to", function(err, parsed) {
     document.createTextNode(parsed.feed.title);
     parsed.feed.entries.forEach(function(entry) {
       // Create elements.
@@ -57,7 +75,7 @@ function rss() {
       // Add hyperlink to the h3
       document.getElementById(titleback).innerHTML = result;
     })
-  })
+  })*/
 }
 function addFeed() {
   var rss = document.getElementById("rssFeedDiv");
@@ -71,4 +89,28 @@ function addFeed() {
     feedAdd.style.display = "block";
     // Show feed adding thing
   }
+}
+
+function edit() {
+  const file = "feeds.json";
+  const jsonfile = require("jsonfile");
+  // Name element
+  var dataone = document.getElementById("yeet").value;
+  // URL element
+  var datatwo = document.getElementById("yeettwo").value;
+
+  // console.log(jsonStr);
+  var obje = JSON.parse(jsonStr);
+
+  obje['feeds'].push({"name":dataone,"url":datatwo});
+  jsonStr = JSON.stringify(obje);
+  // console.log(jsonStr);
+
+  fs.writeFile('feeds.json', jsonStr, (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    };
+    // console.log('File has been written');
+  })
 }
