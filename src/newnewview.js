646d5073de18f902da5abdfdee3dfa4924efc84a
Charles <charles.igel@s.stemk12.org>
2019-04-05 07:06:03 -0600

    Completely working

--- src/newnewview.js
+++ src/newnewview.js
@@ -6,18 +6,14 @@ var content = fs.readFileSync("feeds.json");
 var jsonContent = JSON.parse(content);
 
 var addFeedsHereDiv = document.getElementById("addFeedsHere");
-var feedEntriesDiv = document.getElementById("addEntriesHere"); // Update this
+var feedEntriesDiv = document.getElementById("content"); // Update this
 
 function rssOnLoad() {
   for (let currentFeedd in jsonContent.feeds) {
-
     var feedName = document.createTextNode(jsonContent.feeds[currentFeedd].name);
     var feedEntry = document.createElement("li");
     var a = document.createElement("a");
     var ul = document.getElementById("mainMenu");
-
-    console.log(feedName)
-
     feedEntry.setAttribute("id", "ul-" + currentFeedd);
     ul.appendChild(feedEntry);
     a.setAttribute("href", "javascript:feedClicked('"+jsonContent.feeds[currentFeedd].url+"')");
@@ -28,29 +24,22 @@ function rssOnLoad() {
 }
 
 function feedClicked(url) {
-//  feedEntriesDiv.innerHTML = ''; // clear the div
+  // feedEntriesDiv.innerHTML = ''; // clear the div
   parser.parseURL(url, function(err, parsed) {
     parsed.feed.entries.forEach(function(entry) {
+      var addToMe = document.getElementById("content");
       var text = document.createTextNode(entry.title);
       var entryy = document.createElement("h3");
+
       var titleback = (entry.title).replace(/ /g,"");
       var result = entry.title.link(entry.link);
       //entryy.appendChild(result); // Append text to h3
+
       entryy.setAttribute("id", titleback); // Add id to h3
+
       feedEntriesDiv.appendChild(entryy); // Append h3 to div
+
       document.getElementById(titleback).innerHTML = result;
     })
   });
-}
-
-function toptopkek() {
-  var divExisting = document.getElementById("addEntriesHere");
-
-  var div = document.createElement("div");
-  var ht = document.createElement("h3");
-  var text = document.createTextNode("This is new next.");
-  ht.appendChild(text);
-  div.appendChild(ht);
-
-  divExisting.appendChild(div);
 }
